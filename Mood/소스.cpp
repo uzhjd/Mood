7c7f32fcfbe01db3d7a431686f97ea7b00196cef
uzhjd <73466440+uzhjd@users.noreply.github.com>
2021-11-23 13:12:45 +0900

    카메라 무빙 및 배경화면 픽스

--- Mood/소스.cpp
+++ Mood/소스.cpp
@@ -1,278 +1,184 @@
-//1. 올바른 법선 벡터 적용 여부 확인
-//2. 광원에 적용된 좌표계(월드좌표계, 카메라 좌표계)
-
-#include <windows.h>           
+#include <windows.h>
+#include <gl/gl.h>
+#include <glut.h>
+#include <math.h>
 #include <iostream>
+#include <string>
+#include<MMSystem.h>
+#pragma comment(lib,"Winmm.lib")
 using namespace std;
 
-#include <gl/glut.h>
-#include <math.h>
+//#define score_height	40
+#define	width 			700
+#define	height			700
+int	left_ = 0;
+int	bottom = 0;
+float	camera_distance;
+float	camera_theta, camera_phi;
+float Radius;
+#define PI 3.141592
 
-#define PI   3.1415926
-#define N    36    // Number of parameter “theta” 
-#define M    18    // Number of parameter “phi”  
-
-float theta, phi;
-float delta_theta, delta_phi;
-float start_theta, start_phi;
-
-float	radius;
-float	ver[N][M + 1][3], ver2[N][M + 1][3];
-
-int		camera_setting_flag = 1;
-int		light_type = 1; // right normal computation  
-
-typedef struct postion {
-	float	x;
-	float	y;
-	float	z;
-}position;
+float	lightPositionR[] = { 0.0f, 0.0f, 5.0f, 1.0f };
 
 
-position fixed_sphere, moving;
-
-// 광원 속성
-float light_position[] = { 0.0, 0.0, 0.0, 1.0 };
-float light_ambient[] = { 1.0, 0.0, 0.0, 0.0 };
-float light_diffuse[] = { 1.0, 1.0, 0.0, 0.0 };
-float light_specular[] = { 0.0, 0.0, 1.0, 0.0 };
-
-// 재질 1 : 반사율 
-float ambient0[] = { 1.0, 1.0, 1.0, 0.0 };
-float diffuse0[] = { 1.0, 1.0, 0.0, 0.0 };
-float specular0[] = { 1.0, 1.0, 1.0, 0.0 };
-
-// 재질 2 : 반사율
-float ambient1[] = { 0.0, 1.0, 1.0, 0.0 };
-float diffuse1[] = { 0.0, 1.0, 1.0, 0.0 };
-float specular1[] = { 0.0, 1.0, 1.0, 0.0 };
-
-
-void  VertexGeneration(void) {
-	start_theta = 0.0;
-	delta_theta = 2.0 * PI / N;
-	start_phi = -1.0 * PI / 2.0;
-	delta_phi = 1.0 * PI / M;
-
-	for (int j = 0; j <= M; j++) { // phi (위도)
-		for (int i = 0; i < N; i++) {  // theta (경도)
-			theta = start_theta + i * delta_theta;
-			phi = start_phi + j * delta_phi;
-			ver[i][j][0] = radius * cos(phi) * cos(theta);
-			ver[i][j][1] = radius * cos(phi) * sin(theta);
-			ver[i][j][2] = radius * sin(phi);
-		}
-
+unsigned char* LoadBitmapFile(const char* filename, BITMAPINFOHEADER* bitmapInfoHeader) { // 배경이미지
+	FILE* filePtr;
+	BITMAPFILEHEADER bitmapFileHeader;
+	unsigned char* bitmapImage;
+	int imageIdx = 0;
+	unsigned char tempRGB;
+	errno_t err;
+	err = fopen_s(&filePtr, filename, "rb");
+	if (filePtr == NULL)
+		return NULL;
+	// 비트맵 파일 헤드를 읽는다.
+	fread(&bitmapFileHeader, sizeof(BITMAPFILEHEADER), 1, filePtr);
+	// 윈도우즈 BMP 파일인지 확인한다. 
+	if (bitmapFileHeader.bfType != 'MB') {
+		fclose(filePtr);
+		return (NULL);
 	}
-	for (int j = 0; j <= M; j++) { // phi (위도)
-		for (int i = 0; i < N; i++) {  // theta (경도)
-			theta = start_theta + i * delta_theta;
-			phi = start_phi + j * delta_phi;
-			ver2[i][j][0] = ver[i][j][0] + moving.x;
-			ver2[i][j][1] = ver[i][j][1] + moving.y;
-			ver2[i][j][2] = ver[i][j][2] + moving.z;
-		}
-
+	// 비트맵 정보 헤드를 읽는다.
+	fread(bitmapInfoHeader, sizeof(BITMAPINFOHEADER), 1, filePtr);
+	// 파일 포인터를 비트맵 데이터의 시작 위치로 옮긴다. 
+	fseek(filePtr, bitmapFileHeader.bfOffBits, SEEK_SET);
+	// 비트맵 이미지 데이터를 넣을 메모리를 할당한다. 
+	bitmapImage = (unsigned char*)malloc(bitmapInfoHeader->biSizeImage);
+	//메모리가 제대로 할당되었는지 확인 
+	if (!bitmapImage) {
+		free(bitmapImage);
+		fclose(filePtr);
+		return NULL;
 	}
+	// 메모리에 비트맵 데이트를 읽어드림 
+	fread(bitmapImage, 1, bitmapInfoHeader->biSizeImage, filePtr);
+	// 제대로 들어갔는지 확인 
+	if (bitmapImage == NULL) {
+		fclose(filePtr);
+		return NULL;
+	}
+	//BMP는 BGR형식이므로 R과 B를 서로 교체해야 함
+	for (imageIdx = 0; imageIdx < bitmapInfoHeader->biSizeImage; imageIdx += 3) {
+		tempRGB = bitmapImage[imageIdx];
+		bitmapImage[imageIdx] = bitmapImage[imageIdx + 2];
+		bitmapImage[imageIdx + 2] = tempRGB;
+	}
+	// 파일을 닫고 비트맵 이미지 데이터 반환 
+	fclose(filePtr);
+	return bitmapImage;
+}
+
+BITMAPINFOHEADER bitmapInfoHeader1;
+unsigned char* bitmapImage_1 = LoadBitmapFile("Rapunzel.bmp", &bitmapInfoHeader1);
+BITMAPINFOHEADER bitmapInfoHeader2;
+unsigned char* bitmapImage_2 = LoadBitmapFile("GameOver.bmp", &bitmapInfoHeader2);
+BITMAPINFOHEADER bitmapInfoHeader3;
+unsigned char* bitmapImage_3 = LoadBitmapFile("Pause.bmp", &bitmapInfoHeader3);
+BITMAPINFOHEADER bitmapInfoHeader4;
+unsigned char* bitmapImage_4 = LoadBitmapFile("Start.bmp", &bitmapInfoHeader4);
+BITMAPINFOHEADER bitmapInfoHeader5;
+unsigned char* bitmapImage_5 = LoadBitmapFile("Clear.bmp", &bitmapInfoHeader5);
+
+void init(void) { // 공들의 정보 초기화
+	Radius = 1.0;
+	camera_phi = PI / 6.0;
+	//camera_theta = PI / 4.0;
+	camera_theta = 0.0;
+	camera_distance = 4.0 * Radius;
+	//Game_level = 1;
+	//point = 0;
 
 }
 
-
-
-void DrawSphere1(void) {
-	for (int j = 0; j < M; j++) {
-		for (int i = 0; i < N; i++) {
-			glBegin(GL_POLYGON);
-			glNormal3fv(ver[i][j]);
-			glVertex3fv(ver[i][j]);
-
-			glNormal3fv(ver[(i + 1) % N][j]);
-			glVertex3fv(ver[(i + 1) % N][j]);
-
-			glNormal3fv(ver[(i + 1) % N][j + 1]);
-			glVertex3fv(ver[(i + 1) % N][j + 1]);
-
-			glNormal3fv(ver[i][j + 1]);
-			glVertex3fv(ver[i][j + 1]);
-			glEnd();
-		}
-	}
-}
-
-void DrawSphere2(void) {
-	for (int j = 0; j <= M; j++) { // phi (위도)
-		for (int i = 0; i < N; i++) {  // theta (경도)
-			theta = start_theta + i * delta_theta;
-			phi = start_phi + j * delta_phi;
-			ver2[i][j][0] = ver[i][j][0] + moving.x;
-			ver2[i][j][1] = ver[i][j][1] + moving.y;
-			ver2[i][j][2] = ver[i][j][2] + moving.z;
-		}
-
-	}
-	if (light_type % 2) {
-		for (int j = 0; j < M; j++) {
-			for (int i = 0; i < N; i++) {
-				glBegin(GL_POLYGON);
-				glNormal3fv(ver[i][j]);
-				glVertex3fv(ver2[i][j]);
-
-				glNormal3fv(ver[(i + 1) % N][j]);
-				glVertex3fv(ver2[(i + 1) % N][j]);
-
-				glNormal3fv(ver[(i + 1) % N][j + 1]);
-				glVertex3fv(ver2[(i + 1) % N][j + 1]);
-
-				glNormal3fv(ver[i][j + 1]);
-				glVertex3fv(ver2[i][j + 1]);
-				glEnd();
-			}
-		}
-	}
-	else {
-		for (int j = 0; j < M; j++) {
-			for (int i = 0; i < N; i++) {
-				glBegin(GL_POLYGON);
-				glNormal3fv(ver2[i][j]);
-				glVertex3fv(ver2[i][j]);
-
-				glNormal3fv(ver2[(i + 1) % N][j]);
-				glVertex3fv(ver2[(i + 1) % N][j]);
-
-				glNormal3fv(ver2[(i + 1) % N][j + 1]);
-				glVertex3fv(ver2[(i + 1) % N][j + 1]);
-
-				glNormal3fv(ver2[i][j + 1]);
-				glVertex3fv(ver2[i][j + 1]);
-				glEnd();
-			}
-		}
-	}
-
-}
-
-
-void  init(void) {
-	radius = 1.0;
-	fixed_sphere.x = 0.0;
-	fixed_sphere.y = 0.0;
-	fixed_sphere.z = 0.0;
-
-	moving.x = 3.0;
-	moving.y = 0.0;
-	moving.z = 0.0;
-
-	glEnable(GL_DEPTH_TEST);
-	glShadeModel(GL_SMOOTH);   //glShadeModel(GL_FLAT);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-
-	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-	VertexGeneration();
-}
-
-
-void  reshape(int w, int h) {
+void MyReshape(int w, int h) { // 시점 및 초기화
 	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(45.0, 1.0, 1.0, 1000);
+	gluOrtho2D(left_, left_ + width, bottom, bottom + height);
 }
 
-void Draw_Axis(void) {
+//void Modeling_Score() { // 점수판 만들기
+//	glColor3f(0.8, 0.5, 0.5);
+//	glRectf(0, 0, width, score_height);
+//}
 
-	glLineWidth(3.0);
-	glBegin(GL_LINES);
-	glColor3f(1.0, 0.0, 0.0); // red color
-	glVertex3f(0.0, 0.0, 0.0);
-	glVertex3f(100.0, 0.0, 0.0);
 
-	glColor3f(0.0, 1.0, 0.0); // green color
-	glVertex3f(0.0, 0.0, 0.0);
-	glVertex3f(0.0, 100.0, 0.0);
 
-	glColor3f(0.0, 0.0, 1.0); // blue color
-	glVertex3f(0.0, 0.0, 0.0);
-	glVertex3f(0.0, 0.0, 100.0);
-	glEnd();
-}
+void RenderScene(void) { // 변경 화면
+	float	x, y, z;
 
-void display(void)
-{
-	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+
+	// Camera Position 
+	x = camera_distance * cos(camera_phi) * cos(camera_theta);
+	y = camera_distance * cos(camera_phi) * sin(camera_theta);
+	z = camera_distance * sin(camera_phi);
+
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	if (camera_setting_flag % 2) {
-		glLightfv(GL_LIGHT0, GL_POSITION, light_position);  // light position in camera coordinates system
-		gluLookAt(6.0, 10.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
-	}
-	else {
-		gluLookAt(6.0, 10.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
-		glLightfv(GL_LIGHT0, GL_POSITION, light_position); // light position in world coordinates
-	}
 
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse0); // Yellow Sphere
-	DrawSphere1();
 
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse1); // Green Sphere
-	DrawSphere2();
 
-	glDisable(GL_LIGHTING);
-	Draw_Axis();
-	glEnable(GL_LIGHTING);
 
+
+	glShadeModel(GL_FLAT);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+	glRasterPos2i((4.0 - x) * 20, 0); // 배경화면 위치
+	glDrawPixels(bitmapInfoHeader1.biWidth, bitmapInfoHeader1.biHeight, GL_RGB, GL_UNSIGNED_BYTE, bitmapImage_1);
+
+	glLightfv(GL_LIGHT1, GL_POSITION, lightPositionR); // (lightPositionR[0], lightPositionR[1], lightPositionR[2]) in Camera Coordinates
+	gluLookAt(x, y, z, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
+	//Modeling_Score();
+
+
+	glutPostRedisplay();
+	glutSwapBuffers();
 	glFlush();
 }
 
-void my_keyboeard(unsigned char key, int x, int y) {
-	switch (key) {
+void SpecialKey(int key, int x, int y) {
 
-	case 'a':		light_position[2] += 0.7;
+	switch (key) {
+	case GLUT_KEY_LEFT:
+		camera_distance += 0.1;
+		cout << "left" << endl;
+		cout << camera_distance << endl;
+
 		break;
-	case 'd':		light_position[2] -= 0.7;
+	case GLUT_KEY_RIGHT:
+
+
+		camera_distance -= 0.1;
+		cout << "right" << endl;
+		cout << camera_distance << endl;
 		break;
-	case 'l':		light_type++;
+
+	default:
 		break;
-	case 'c':		camera_setting_flag++;
-		break;
-	default: break;
 	}
 	glutPostRedisplay();
-}
 
-void my_special(int key, int x, int y) {
-	switch (key) {
-	case GLUT_KEY_LEFT:		moving.x -= 0.1;
-		break;
-	case GLUT_KEY_RIGHT:	moving.x += 0.1;
-		break;
-	case GLUT_KEY_DOWN:		moving.y -= 0.1;
-		break;
-	case GLUT_KEY_UP:		moving.y += 0.1;
-		break;
-	default: break;
-	}
-	glutPostRedisplay();
 }
 
 
 
-void main(void)
-{
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(500, 500);
-	glutCreateWindow("Two Spheres under camera position change");
+void main(int argc, char** argv) {
+	glutInitWindowPosition(800, 100);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); // 애니메이션처럼 보이기 위해 DOUBLE로 씀
+	glutInitWindowSize(width, height);
+	glutCreateWindow("To the dawn");
 	init();
-	glutDisplayFunc(display);
-	glutKeyboardFunc(my_keyboeard);
-	glutSpecialFunc(my_special);
-	glutReshapeFunc(reshape);
+	glutReshapeFunc(MyReshape); // 범위 설정
+	glutDisplayFunc(RenderScene);
+	glutIdleFunc(RenderScene); // 아무일을 수행 안해도 그것을 이벤트로 삼아라!!!(마우스, 키보드 입력이 없을때 발생)
+
+	glutSpecialFunc(SpecialKey);
+	//	
+	//	//if (you) {
+	//	//	sndPlaySoundA("music.wav", SND_ASYNC | SND_NODEFAULT | SND_LOOP);
+	////
+	//	//}
 	glutMainLoop();
 }
